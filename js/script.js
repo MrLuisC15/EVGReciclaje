8faad70201e7f7537c6273a230797b8818be6c78
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-12-01 08:37:00 +0100

    V1.2 - Nuevos niveles e items

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /** 
- * @file Script para el minijuego de reciclaje
+ * @file Script para el minijuego de reciclaje.
  * @author Luis Carlos Marzal de la Concepción
  * @author Diego Carrión Rodríguez
  * @author Kilian Benavente Ortega
@@ -9,20 +9,24 @@
 'use strict'
 
 /**
- * Instanciamos la puntación ,velocidad, nivel de juego y los sonidos
+ * Instanciamos la puntación ,velocidad, nivel de juego y los sonidos.
  */
 let puntos = 0
 let nivel = 1
-let velocidad=1500
+let velocidad = 2000
+let pausa = 0
 let generadorItems = null
+let contadorPerder= 0
 let acierto = document.createElement("AUDIO");
-acierto.setAttribute("src","acierto.mp3");
+acierto.setAttribute("src","sonidos/acierto.mp3");
 let fallo = document.createElement("AUDIO");
-fallo.setAttribute("src","fallo.mp3");
+fallo.setAttribute("src","sonidos/perder.mp3");
 let pasarNivel = document.createElement("AUDIO");
-pasarNivel.setAttribute("src","pasarNivel.mp3");
+pasarNivel.setAttribute("src","sonidos/pasarNivel.mp3");
 let perder = document.createElement("AUDIO");
-perder.setAttribute("src","perder.mp3");
+perder.setAttribute("src","sonidos/lose.mp3");
+const MUSICAFONDO=document.querySelector("#musica-fondo");
+MUSICAFONDO.loop=true;
 /*
 let acierto = new Audio('../sonidos/acierto.wav')
 let fallo = new Audio('../sonidos/fallo.wav')
@@ -30,17 +34,19 @@ let pasarNivel = new Audio('../sonidos/pasarNivel.wav')
 let perder = new Audio('../sonidos/perder.wav')
 */
 /**
- *Función que permitirá arrastar un objeto
+ * @function allowDrop
+ * @description Función que permitirá arrastar un objeto.
  *
- * @param {*} ev Evento de la función
+ * @param {Event} ev Evento de la función.
  */
 function allowDrop(ev) {
    ev.preventDefault();
 }
 /**
- *Función que gestionará el objeto mientras lo arrastremos
+ * @function drag
+ * @description Función que gestionará el objeto mientras lo arrastremos.
  *
- * @param {*} ev Evento de la función
+ * @param {Event} ev Evento de la función.
  */
 function drag(ev) {
    ev.dataTransfer.setData("text", ev.target.id);
@@ -50,9 +56,10 @@ function drag(ev) {
 }
 
 /**
- *Función que gestionará el objeto mientras lo arrastremos
+ * @function clickItem
+ * @description Función que gestionará el objeto mientras lo cliquemos.
  *
- * @param {*} ev Evento de la función
+ * @param {Event} ev Evento de la función.
  */
  function clickItem(ev) {
    //ev.dataTransfer.setData("text", ev.target.id);
@@ -64,9 +71,10 @@ function drag(ev) {
 }
 
 /**
- *Función que gestiona cuando soltamos un objeto en el contenedor amarillo
+ * @function dropAmarillo
+ * @description Función que gestiona cuando soltamos un objeto en el contenedor amarillo.
  *
- * @param {*} ev Basura que introducimos en el contenedor. Si está en el contendor correcto será borrada, si no, expulsada
+ * @param {Event} ev Basura que introducimos en el contenedor. Si está en el contendor correcto será borrada, si no, expulsada.
  */
 function dropAmarillo(ev) {
    ev.preventDefault();
@@ -84,13 +92,16 @@ function dropAmarillo(ev) {
          ev.target.removeChild(document.getElementById(data));
          acierto.play()
          masPunto()
+         contadorPerder--
          papelera.style.backgroundColor = "greenyellow"
          
       }
       else {
          fallo.play()
          papelera.style.backgroundColor = "red"
-         
+         puntos=puntos-nivel
+         let divPuntos=document.getElementById('puntos')
+         divPuntos.innerHTML = puntos
       }
 
       setTimeout(() => {
@@ -99,9 +110,10 @@ function dropAmarillo(ev) {
    }
 }
 /**
- *Función que gestiona cuando soltamos un objeto en el contenedor azul
+ * @function dropAzul
+ * @description Función que gestiona cuando soltamos un objeto en el contenedor azul.
  *
- * @param {*} ev Basura que introducimos en el contenedor. Si está en el contendor correcto será borrada, si no, expulsada
+ * @param {Event} ev Basura que introducimos en el contenedor. Si está en el contendor correcto será borrada, si no, expulsada.
  */
 function dropAzul(ev) {
    ev.preventDefault();
@@ -118,13 +130,16 @@ function dropAzul(ev) {
          ev.target.removeChild(document.getElementById(data));
          acierto.play()
          masPunto()
+         contadorPerder--
          papelera.style.backgroundColor = "greenyellow"
          
       }
       else {
          fallo.play()
          papelera.style.backgroundColor = "red"
-         
+         puntos=puntos-nivel
+         let divPuntos=document.getElementById('puntos')
+         divPuntos.innerHTML = puntos
       }
 
       setTimeout(() => {
@@ -133,9 +148,10 @@ function dropAzul(ev) {
    }
  }
 /**
- *Función que gestiona cuando soltamos un objeto en el contenedor verde
+ * @function dropVerde
+ * @description Función que gestiona cuando soltamos un objeto en el contenedor verde.
  *
- * @param {*} ev Basura que introducimos en el contenedor. Si está en el contendor correcto será borrada, si no, expulsada
+ * @param {Event} ev Basura que introducimos en el contenedor. Si está en el contendor correcto será borrada, si no, expulsada.
  */
  function dropVerde(ev) {
    ev.preventDefault();
@@ -153,13 +169,16 @@ function dropAzul(ev) {
          ev.target.removeChild(document.getElementById(data));
          acierto.play()
          masPunto()
+         contadorPerder--
          papelera.style.backgroundColor = "greenyellow"
          
       }
       else {
          fallo.play()
          papelera.style.backgroundColor = "red"
-         
+         puntos=puntos-nivel
+         let divPuntos=document.getElementById('puntos')
+         divPuntos.innerHTML = puntos
       }
 
       setTimeout(() => {
@@ -171,48 +190,124 @@ function dropAzul(ev) {
    
 
 /**
- *Suma un punto extra a la puntuación total
+ * @function masPunto
+ * @description Suma puntos extra a la puntuación total y comprueba si hay que subir de nivel.
  */
 function masPunto(){
-   puntos=puntos+1
+   puntos=puntos+1*nivel
    let divPuntos=document.getElementById('puntos')
    divPuntos.innerHTML = puntos
 
    let divNivel=document.getElementById('nivel')
    divNivel.innerHTML = nivel
    //console.log(puntos);
-   if(puntos==5 && nivel==1) {
+   if(puntos>=15 && nivel==1) {
       pasarNivel.play()
-      puntos=1
       nivel=2
-      velocidad=1000 //1000
+      velocidad=1500
+      puntos=15
+      MUSICAFONDO.playbackRate=1.1;
       divPuntos.innerHTML = puntos
       divNivel.innerHTML = nivel
       document.body.style.backgroundImage= 'url(img/fondoN2.jpg)'
       document.getElementById('divPrincipal').style.borderColor = 'red'
       document.getElementById('papeleras').style.borderColor = 'red'
    }
-   if(puntos==15 && nivel==2) {
+   if(puntos>=50 && nivel==2) {
       pasarNivel.play()
-      puntos=1
       nivel=3
-      velocidad=750
+      velocidad=1250
+      puntos=50
+      MUSICAFONDO.playbackRate=1.25;
       divPuntos.innerHTML = puntos
       divNivel.innerHTML = nivel
       document.body.style.backgroundImage= 'url(img/fondoN3.jpg)'
       document.getElementById('divPrincipal').style.borderColor = 'blue'
       document.getElementById('papeleras').style.borderColor = 'blue'
    }
-   if(puntos==35 && nivel==3) {
+   if(puntos>=100 && nivel==3) {
       pasarNivel.play()
-      puntos=1
       nivel=4
-      velocidad=500
+      velocidad=1100
+      puntos=100
+      MUSICAFONDO.playbackRate=1.5;
       divPuntos.innerHTML = puntos
       divNivel.innerHTML = nivel
       document.body.style.backgroundImage= 'url(img/fondoN4.jpg)'
       document.getElementById('divPrincipal').style.borderColor = 'purple'
       document.getElementById('papeleras').style.borderColor = 'purple'
+   }
+   if(puntos>=200 && nivel==4) {
+      pasarNivel.play()
+      nivel=5
+      velocidad=1000
+      puntos=200
+      MUSICAFONDO.playbackRate=1.75;
+      divPuntos.innerHTML = puntos
+      divNivel.innerHTML = nivel
+      document.body.style.backgroundImage= 'url(img/fondoN5.jpg)'
+      document.getElementById('divPrincipal').style.borderColor = 'magenta'
+      document.getElementById('papeleras').style.borderColor = 'magenta'
+   }
+   if(puntos>=350 && nivel==5) {
+      pasarNivel.play()
+      nivel=6
+      velocidad=850
+      puntos=350
+      MUSICAFONDO.playbackRate=2;
+      divPuntos.innerHTML = puntos
+      divNivel.innerHTML = nivel
+      document.body.style.backgroundImage= 'url(img/fondoN6.jpg)'
+      document.getElementById('divPrincipal').style.borderColor = 'yellow'
+      document.getElementById('papeleras').style.borderColor = 'yellow'
+   }
+   if(puntos>=500 && nivel==6) {
+      pasarNivel.play()
+      nivel=7
+      velocidad=700
+      puntos=500
+      MUSICAFONDO.playbackRate=2.25;
+      divPuntos.innerHTML = puntos
+      divNivel.innerHTML = nivel
+      document.body.style.backgroundImage= 'url(img/fondoN7.jpg)'
+      document.getElementById('divPrincipal').style.borderColor = 'brown'
+      document.getElementById('papeleras').style.borderColor = 'brown'
+   }
+   if(puntos>=1000 && nivel==7) {
+      pasarNivel.play()
+      nivel=8
+      velocidad=550
+      puntos=1000
+      MUSICAFONDO.playbackRate=2.5;
+      divPuntos.innerHTML = puntos
+      divNivel.innerHTML = nivel
+      document.body.style.backgroundImage= 'url(img/fondoN8.jpg)'
+      document.getElementById('divPrincipal').style.borderColor = 'cyan'
+      document.getElementById('papeleras').style.borderColor = 'cyan'
+   }
+   if(puntos>=2000 && nivel==8) {
+      pasarNivel.play()
+      nivel=9
+      velocidad=400
+      puntos=2000
+      MUSICAFONDO.playbackRate=2.75;
+      divPuntos.innerHTML = puntos
+      divNivel.innerHTML = nivel
+      document.body.style.backgroundImage= 'url(img/fondoN9.jpg)'
+      document.getElementById('divPrincipal').style.borderColor = 'pink'
+      document.getElementById('papeleras').style.borderColor = 'pink'
+   }
+   if(puntos>=5000 && nivel==9) {
+      pasarNivel.play()
+      nivel=10
+      velocidad=250
+      puntos=5000
+      MUSICAFONDO.playbackRate=3;
+      divPuntos.innerHTML = puntos
+      divNivel.innerHTML = nivel
+      document.body.style.backgroundImage= 'url(img/fondoN10.jpg)'
+      document.getElementById('divPrincipal').style.borderColor = 'black'
+      document.getElementById('papeleras').style.borderColor = 'black'
    }
 }
 
@@ -233,7 +328,9 @@ class Juego{
 
 
    /**
-    * Pone en marcha el juego
+    * @function iniciar
+    * @description Pone en marcha el juego.
+    * @memberof Juego
     */
    iniciar(){
       console.log('Iniciando...');
@@ -243,32 +340,113 @@ class Juego{
       //console.log(this);
 
    }
-
+   /**
+    * @function intervaloItem
+    * @description Establece la velocidad a la que aparecerán los objetos.
+    * @memberof Juego
+    */
    intervaloItem(){
       generadorItems= window.setInterval(this.generarItem.bind(this), velocidad)
      
    }
-
    /**
-    *Crea un nuevo objeto basura
-    *
+    * @function generarItem
+    * @description Crea un nuevo objeto basura.
     * @memberof Juego
     */
    generarItem(){
       let nuevoItem = this.modelo.crearItem()
       let contadorItems = 0
-      this.vista.dibujar(divPrincipal, nuevoItem, contadorItems)
-      contadorItems++
-      if((nivel==2 || nivel==3 || nivel==4)&& puntos==1){
+      let divPuntos=document.getElementById('puntos')
+
+      if(pausa==2) {
+         contadorPerder++
+         
+         this.vista.dibujar(divPrincipal, nuevoItem, contadorItems)
+         contadorItems++
+         this.comprobarPerder()
+         
+      }
+      if(((puntos==15 && nivel==2) || (puntos==50 && nivel==3) ||(puntos==100 && nivel==4) || (puntos==200 && nivel==5) || (puntos==350 && nivel==6) || (puntos==500 && nivel==7) || (puntos==1000 && nivel==8) || (puntos==2000 && nivel==9) || (puntos==5000 && nivel==10) )|| pausa==1){
          window.clearInterval(generadorItems)
          this.intervaloItem()
+         this.limpiarPantalla()
+         puntos=puntos+nivel
+         divPuntos.innerHTML = puntos
+         pausa=2
+      }
+   }
+   /**
+    * @function comprobarPerder
+    * @description Cuenta cuánta basura hay para ver si debe mostrar o no la pantalla de Game Over.
+    * @memberof Juego
+    */
+   comprobarPerder(){
+      if(contadorPerder>=10 && nivel==1) {
+         this.perder()
+      }
+      if(contadorPerder>=20 && nivel==2) {
+         this.perder()
+      }
+      if(contadorPerder>=30 && nivel==3) {
+         this.perder()
+      }
+      if(contadorPerder>=40 && nivel==4) {
+         this.perder()
+      }
+      if(contadorPerder>=50 && nivel==5) {
+         this.perder()
+      }
+      if(contadorPerder>=60 && nivel==6) {
+         this.perder()
+      }
+      if(contadorPerder>=80 && nivel==7) {
+         this.perder()
+      }
+      if(contadorPerder>=100 && nivel==8) {
+         this.perder()
+      }
+      if(contadorPerder>=120 && nivel==9) {
+         this.perder()
+      }
+      if(contadorPerder>=140 && nivel==10) {
+         this.perder()
       }
    }
 
+   /**
+    * @function perder
+    * @description Muestra la pantalla de Game Over.
+    * @memberof Juego
+    */
+   perder(){
+      window.clearInterval(generadorItems)
+      perder.play()
+      this.limpiarPantalla()
+      this.divPrincipal
+      let divPerder= document.createElement('div')
+      let img = document.createElement('img')
+      img.setAttribute('src', 'img/GameOver.png');
+      document.body.appendChild(divPerder);
+      divPerder.classList.add('gameOver');
+      divPerder.appendChild(img);
+
+
+
+
+      //divPerder.textContent=`Has perdido en el nivel ${nivel}`
+      pausa=0
+   }
+   
+   limpiarPantalla(){
+      while(this.divPrincipal.childElementCount>0) {
+         this.divPrincipal.removeChild(this.divPrincipal.children[0])
+      }
+   }
   
 }
 /**
- *Clase vista que muestra el juego
+ *Clase vista que muestra el juego.
  *
  * @class Vista
  */
@@ -276,17 +454,17 @@ class Vista{
    constructor(contador){
       //this.contadorTop = 10
       this.contadorItems=contador
-      this.itemsAmarillo = ['lata.png']
-      this.itemsAzul = ['carton.png','pelota.png']
-      this.itemsVerde = [ 'mansana.png']
+      this.itemsAmarillo = ['lata.png','actimel.png','plasticoPajita.png','pelota.png','bebidoMora.png','bollicao.png','zumito.png','zumotropical.png']
+      this.itemsAzul = ['carton.png','pajita.png','bolaPapel.png']
+      this.itemsVerde = [ 'manzana.png','platano.png','naranja.png','melocoton.png','kiwi.png']
    }
 
    /**
-    *Dibuja los elementos del juego
-    *
-    * @param {*} divPrincipal Contenedor donde aperecerá la basura
-    * @param {*} nuevoItem Objeto basura que se introducirá en el contenedor
-    * @param {*} contadorItems Variable que llevará la cuenta de toda la basura que hay
+    * @function dibujar
+    * @description Dibuja los elementos del juego.
+    * @param {HTMLDivElement} divPrincipal Contenedor donde aperecerá la basura.
+    * @param {String} nuevoItem Objeto basura que se introducirá en el contenedor.
+    * @param {BigInteger} contadorItems Variable que llevará la cuenta de toda la basura que hay.
     * @memberof Vista
     */
    dibujar(divPrincipal, nuevoItem, contadorItems){
@@ -300,7 +478,14 @@ class Vista{
       let clase = this.preguntaTipo(nuevoItem);
       img.classList.add(clase)
 
-      img.style.top = Math.floor(Math.random() *290)+'px'
+      //img.style.top = Math.floor(Math.random() *290)+'px'
+      if(Math.floor(Math.random() *2)==1) { //
+         img.style.top = Math.floor(Math.random() *50)+'%'
+      }
+      else{
+         img.style.bottom = Math.floor(Math.random() *50)+'%'
+      }
+
       if(Math.floor(Math.random() *2)==1) { //
          img.style.left = Math.floor(Math.random() *50)+'%'
       }
@@ -319,10 +504,10 @@ class Vista{
       
    }
    /**
-    *Valida a qué contenedor debe ir cada objeto basura
-    *
-    * @param {*} nuevoItem Objeto basura creado
-    * @return {*} Contenedor al que debe ir
+    * @function preguntaTipo
+    * @description Valida a qué contenedor debe ir cada objeto basura.
+    * @param {String} nuevoItem Objeto basura creado.
+    * @return {String} Contenedor al que debe ir.
     * @memberof Vista
     */
    preguntaTipo(nuevoItem){
@@ -349,20 +534,20 @@ class Vista{
    }
 }
 /**
- *Clase Modelo encargada de la parte lógica del programa
+ *Clase Modelo encargada de la parte lógica del programa.
  *
  * @class Modelo
  */
 class Modelo{
    constructor(){
-      this.itemsAmarillo = ['lata.png']
-      this.itemsAzul = ['carton.png','pelota.png']
-      this.itemsVerde = [ 'mansana.png']
+      this.itemsAmarillo = ['lata.png','actimel.png','plasticoPajita.png','bebidoMora.png','bollicao.png','zumito.png','zumotropical.png']
+      this.itemsAzul = ['carton.png','pelota.png','pajita.png','bolaPapel.png']
+      this.itemsVerde = [ 'manzana.png','platano.png','naranja.png','melocoton.png','kiwi.png']
    }
    /**
-    *Crea un nuevo objeto basura
-    *
-    * @return {*} Objeto basura
+    * @function crearItem
+    * @description Crea un nuevo objeto basura.
+    * @return {*} Objeto basura.
     * @memberof Modelo
     */
    crearItem(){
@@ -411,7 +596,21 @@ draggable.setAttribute("ondragstart","drag(event)")
 draggable2.setAttribute("ondragstart","drag(event)")
 draggable3.setAttribute("ondragstart","drag(event)")
 */
-  
+
+let boton = document.getElementById('bIniciar')
+boton.setAttribute("onclick", "clickIniciar()")
+
+/**
+ * @function clickIniciar
+ * @description Inicia el juego al hacer click.
+ */
+function clickIniciar() {
+   MUSICAFONDO.play();
+   let boton = document.getElementById('bIniciar')
+   boton.style.display = 'none'
+   pausa=1
+}
 
 var app = new Juego()
+
 
